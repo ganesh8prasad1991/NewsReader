251c6d5507a33fb82072b18df0b821cf06e415f1
ganesh8prasad1991 <ganesh8prasad1991@gmail.com>
2024-09-14 10:04:52 +0530

    Initial Commit

---
+++ NewsReader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewsReader
+//
+//  Created by Ganesh Prasad on 14/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewsReader/NewsReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsReaderApp.swift
+//  NewsReader
+//
+//  Created by Ganesh Prasad on 14/09/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
